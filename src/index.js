b0cb67417549664ccf9400264a73a199a913e92d
王东兴 <1024122166@qq.com>
2022-09-06 16:46:40 +0800

    Bug：点到线段距离，线段起始点和结束点为同一点计算点与线段位置向量为NAN问题

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
  * @param lienEnd
  */
  export function getTheDistanceOfAPointLineSegment(point, lineStart, lienEnd) {
+    if(lineStart.x === lienEnd.x && lineStart.y === lienEnd.y) return Math.sqrt((point.x - lineStart.x) * (point.x - lineStart.x) + (point.y - lineStart.y) * (point.y - lineStart.y));
     let r = ((point.x - lineStart.x) * (lienEnd.x - lineStart.x) + (point.y - lineStart.y) * (lienEnd.y - lineStart.y))
         / ((lienEnd.x - lineStart.x) * (lienEnd.x - lineStart.x) + (lienEnd.y - lineStart.y) * (lienEnd.y - lineStart.y));
     if (r <= 0)return Math.sqrt((point.x - lineStart.x) * (point.x - lineStart.x) + (point.y - lineStart.y) * (point.y - lineStart.y));
@@ -13,6 +14,7 @@
     let py = lineStart.y + (lienEnd.y - lineStart.y) * r;
     return Math.sqrt((point.x-px)*(point.x-px)+(point.y-py)*(point.y-py));
 }
+
 
 /**
  * 确定一个点是否在另一个点区域内
