817a98eaacfc2d7fe46d643905044b0974926ac0
王东兴 <1024122166@qq.com>
2022-09-24 15:40:27 +0800

    添加测试用例

---
+++ src/__test__/Graphic.test.ts
@@ -0,0 +1,14 @@
+import { getTheDistanceFromPointToLine, getTheDistanceOfAPointLineSegment, determineIfAPointIsWithinAnotherPointArea } from "../core/graphic";
+import { Point } from "../core/graphic/Point";
+
+test('getTheDistanceOfAPointLineSegment', ()=>{
+  expect(getTheDistanceOfAPointLineSegment(new Point(-5 ,0), new Point(0, 0), new Point(2, 0))).toBe(5)
+})
+
+test('getTheDistanceFromPointToLine', ()=>{
+  expect(getTheDistanceFromPointToLine(new Point(-6,1), new Point(0, 0), new Point(2, 0))).toBe(1)
+})
+
+test('determineIfAPointIsWithinAnotherPointArea', ()=>{
+  expect(determineIfAPointIsWithinAnotherPointArea(new Point(1, 1),new Point(0, 0))).toBe(true)
+})
