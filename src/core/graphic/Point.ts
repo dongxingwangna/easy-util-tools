ea693e07d0d269b4613b022d5fc7345e5e610f60
dongxingwang <dongxingwang.na@gmail.com>
2023-03-24 14:12:20 +0800

    Modify the Point class

--- src/core/graphic/Point.ts
+++ src/core/graphic/Point.ts
@@ -1,3 +1,6 @@
+import {getTheDistanceBetweenTwoPoints} from "./index";
+import {PointInfo} from "./PointInfo";
+
 /**
  * @docName: Point.ts
  * @Author: wdx
@@ -36,4 +39,36 @@ export class Point {
   static isPoint(point: Point): boolean {
     return point.type === 'Point';
   }
+
+  /**
+   * 获取到点的距离
+   * Gets the distance to the point
+   * @param point
+   */
+  getDistanceToPoint(point:Point):number {
+    return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
+  }
+
+  /**
+   * 获取距离最近的点
+   * Gets the closest point
+   * @param points
+   */
+  getClosestPoint(points:Point[]):PointInfo{
+    let index: number = 0;
+    const data: Point = new Point(points[0].x, points[0].y);
+    const dif: number = getTheDistanceBetweenTwoPoints(this, points[0]);
+    for (let i = 0; i < points.length; i++) {
+      if (getTheDistanceBetweenTwoPoints(this, points[i]) < dif) {
+        index = i;
+        data.x = points[i].x;
+        data.y = points[i].y;
+      }
+    }
+    return {
+      index,
+      data,
+    };
+  }
+
 }
